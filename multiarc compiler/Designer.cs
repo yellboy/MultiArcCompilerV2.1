f461eb596bb88592c5b885f4cf4c06c03e215c4e
Bojan <bojan.jelaca@zuehlke.com>
2016-03-15 23:20:34 +0100

    Started working on removing white space on image

--- multiarc compiler/Designer.cs
+++ multiarc compiler/Designer.cs
@@ -37,12 +37,45 @@ namespace MultiArc_Compiler
         private void BrowseComponentImageDialog_FileOk(object sender, CancelEventArgs e)
         {
             var image = new Bitmap(BrowseComponentImageDialog.FileName);
+            var graphics = DesignPanel.CreateGraphics();
             if (MessageBox.Show("Do you want to make this image transparent?", "Transparent", MessageBoxButtons.YesNo) == DialogResult.Yes)
             {
+                // TODO Fix this algorhitm
+                int upperBorder = 0;
+                int leftBorder = 0;
+                int lowerBorder = 0;
+                int rightBorder = 0;
+                for (int x = 0; x < image.Size.Width; x++)
+                {
+                    for (int y = 0; y < image.Size.Height; y++)
+                    {
+                        var pixel = image.GetPixel(x, y);
+                        var color = pixel.ToArgb();
+                        var white = Color.White.ToArgb();
+                        if (color != white)
+                        {
+                            if (y > upperBorder)
+                            {
+                                upperBorder = y;
+                            }
+                            if (x > leftBorder)
+                            {
+                                leftBorder = x;
+                            }
+                        }
+                        else
+                        {
+                            { }
+                        }
+                    }
+                }
                 image.MakeTransparent();
+                graphics.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height), new Rectangle(leftBorder, upperBorder, rightBorder - leftBorder, lowerBorder - upperBorder), GraphicsUnit.Pixel);
             }
-            var graphics = DesignPanel.CreateGraphics();
-            graphics.DrawImage(image, new Point(0, 0));
+            else
+            {
+                graphics.DrawImage(image, new Point(0, 0));
+            }
         }
 
         private void BrowseComponentImageButton_Click(object sender, EventArgs e)
